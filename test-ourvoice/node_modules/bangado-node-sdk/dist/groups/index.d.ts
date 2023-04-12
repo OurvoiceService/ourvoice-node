4bf3cd03d6ae836629122aec2804a8f7a5d9fd86
Bangado <jules2bessan@gmail.com>
2023-04-12 12:20:05 +0100

    build

--- test-ourvoice/node_modules/bangado-node-sdk/dist/groups/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Base } from "../base";
-import { Group, NewGroup } from "./type";
-export declare class Groups extends Base {
-    getGroupById(id: string): Promise<Group>;
-    getGroups(): Promise<any>;
-    createGroup(newGroup: NewGroup): Promise<Group>;
-}
--- test-ourvoice/node_modules/bangado-node-sdk/dist/resources/sms/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { Base } from "../base";
-import { NewSms, Sms } from "./type";
-export declare class SMS extends Base {
-    getSmsById(id: string): Promise<Sms>;
-    getSms(): Promise<any>;
-    sendSms(newSms: NewSms): Promise<Sms>;
-}
