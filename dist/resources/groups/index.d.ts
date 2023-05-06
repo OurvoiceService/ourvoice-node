78e33a1ba5f393e47a23b0ceba53fada96cb6c54
asiedu-kev <asiedukevin050@gmail.com>
2023-05-06 13:11:40 +0100

    :fire: Remove useless files from git tracking

--- dist/resources/groups/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Base } from "../base";
-import { Group, NewGroup } from "./type";
-export declare class Groups extends Base {
-    getGroupById(id: string): Promise<Group>;
-    getGroups(): Promise<any>;
-    createGroup(newGroup: NewGroup): Promise<any>;
-    deleteGroup(id: string): Promise<any>;
-    updateGroup(id: string, group: Partial<Group>): Promise<Group>;
-}
